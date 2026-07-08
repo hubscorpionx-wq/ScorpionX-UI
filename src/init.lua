4d22bb3a714e4606e5d62539463c3f440294a38d
hubscorpionx-wq <hubscorpionx@gmail.com>
2026-07-08 17:06:06 +0200

    Update init.lua

--- src/init.lua
+++ src/init.lua
@@ -1,1 +1,14 @@
+local Library = {}
 
+Library.Theme = require(script.Theme)
+Library.Window = require(script.Window)
+Library.Tabs = require(script.Tabs)
+Library.Elements = require(script.Elements)
+Library.Notifications = require(script.Notifications)
+Library.Utils = require(script.Utils)
+
+function Library:CreateWindow(options)
+    return self.Window.new(options)
+end
+
+return Library
